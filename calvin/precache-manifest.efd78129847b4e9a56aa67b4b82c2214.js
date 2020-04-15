e9adb5f619133b5348c8b4270cdcb4e78d76879f
Hotheda <59983366+Hotheda@users.noreply.github.com>
2020-04-15 12:44:32 +0200

    Add files via upload

---
+++ calvin/precache-manifest.efd78129847b4e9a56aa67b4b82c2214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8d186a87707ca87dc33f1865ec02ca",
+    "url": "/calvin/index.html"
+  },
+  {
+    "revision": "5d492a3219ac6162546f",
+    "url": "/calvin/static/css/main.1bcd6fef.chunk.css"
+  },
+  {
+    "revision": "6858b223b1825849fad7",
+    "url": "/calvin/static/js/2.26a87168.chunk.js"
+  },
+  {
+    "revision": "5d492a3219ac6162546f",
+    "url": "/calvin/static/js/main.33d5f112.chunk.js"
+  },
+  {
+    "revision": "65f572869e281937e43c",
+    "url": "/calvin/static/js/runtime~main.b6a2d425.js"
+  }
+]);
