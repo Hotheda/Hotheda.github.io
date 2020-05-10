f4602b58fd9fd47f4d09d3333a40eb84b3c75e31
Hotheda <59983366+Hotheda@users.noreply.github.com>
2020-05-11 01:44:56 +0200

    Add files via upload

---
+++ calvin/precache-manifest.0ee34295054eaec7862b29da4f414b54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9b455fe647a8bfc47343bb47ebebfa",
+    "url": "/calvin/index.html"
+  },
+  {
+    "revision": "977da46294ef337a014a",
+    "url": "/calvin/static/css/main.d230029b.chunk.css"
+  },
+  {
+    "revision": "309ea6021d1875e58fd5",
+    "url": "/calvin/static/js/2.040708a3.chunk.js"
+  },
+  {
+    "revision": "977da46294ef337a014a",
+    "url": "/calvin/static/js/main.71dc21d5.chunk.js"
+  },
+  {
+    "revision": "65f572869e281937e43c",
+    "url": "/calvin/static/js/runtime~main.b6a2d425.js"
+  }
+]);
--- calvin/service-worker.js
+++ calvin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calvin/precache-manifest.efd78129847b4e9a56aa67b4b82c2214.js"
+  "/calvin/precache-manifest.0ee34295054eaec7862b29da4f414b54.js"
 );
 
 self.addEventListener('message', (event) => {
